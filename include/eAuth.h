c01bc3f2c638dd6b427935c479c4f8b69cd8c602
Esteban <acevedoesteban999@gmail.com>
2025-01-09 09:18:58 -0500

    FIX: js css route

--- include/eAuth.h
+++ include/eAuth.h
@@ -17,14 +17,14 @@
 #define EAUTH_HANDLERS_WITHOUT_STATIC(handler_html,login_html_asm_start,login_html_asm_end) \
     {{"/login.html", HTTP_GET , handler_html , NULL}, true, {login_html_asm_start,login_html_asm_end,""}}, \
     {{"/logout", HTTP_GET , eauth_logout_handler , NULL}, false, {}}, \
-    {{"/login", HTTP_POST , eauth_login_post_handler , NULL}, false, {}}, \
+    {{"/login", HTTP_POST , eauth_login_post_handler , NULL}, false, {}} \
 
 #define EAUTH_HANDLERS_WITH_STATIC(handler_html,handler_statics,login_html_asm_start,login_html_asm_end,login_css_asm_start,login_css_asm_end,login_js_asm_start,login_js_asm_end) \
     {{"/login.html", HTTP_GET , handler_html , NULL}, true, {login_html_asm_start,login_html_asm_end,""}}, \
-    {{"/login.css", HTTP_GET , handler_statics , NULL}, true, {login_css_asm_start,login_css_asm_end,"text/css"}}, \
-    {{"/login.js", HTTP_GET , handler_statics , NULL}, true, {login_js_asm_start,login_js_asm_end,"texr/javascript"}}, \
+    {{"/css/login.css", HTTP_GET , handler_statics , NULL}, true, {login_css_asm_start,login_css_asm_end,"text/css"}}, \
+    {{"/js/login.js", HTTP_GET , handler_statics , NULL}, true, {login_js_asm_start,login_js_asm_end,"texr/javascript"}}, \
     {{"/logout", HTTP_GET , eauth_logout_handler , NULL}, false, {}}, \
-    {{"/login", HTTP_POST , eauth_login_post_handler , NULL}, false, {}}, \
+    {{"/login", HTTP_POST , eauth_login_post_handler , NULL}, false, {}} \
 
 // Estructura para almacenar información de un usuario
 typedef struct {
